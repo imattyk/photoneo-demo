03dac313ccc41bd54f48ed8b1966ba93c6cb48c1
Matej Komlóši <matej.komlosi@gmail.com>
2023-04-23 11:56:29 +0200

    Components created

---
+++ client/src/Components/Canvas/Canvas.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Canvas(props) {
+  return (
+    <div>Canvas</div>
+  )
+}
+
+Canvas.propTypes = {}
+
+export default Canvas
---
+++ client/src/Components/Inputs/Inputs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Inputs(props) {
+  return (
+    <div>Inputs</div>
+  )
+}
+
+Inputs.propTypes = {}
+
+export default Inputs
